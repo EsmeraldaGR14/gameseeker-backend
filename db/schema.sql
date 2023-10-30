60cc84d920de28416deb0c4230b448f3c1483262
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air-5.lan>
2023-10-30 18:29:50 -0400

    created the schema for games table and put in some mockup data

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,25 @@ DROP DATABASE IF EXISTS gameseeker_dev;
 CREATE DATABASE gameseeker_dev;
 
 \c gameseeker_dev;
+
+DROP TABLE IF EXISTS games;
+
+CREATE TABLE game (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    rating DECIMAL(3,1),
+    description TEXT,
+    platform VARCHAR,
+    boxart VARCHAR,
+    ESRB VARCHAR,
+    subscription_service VARCHAR,
+    released_year INTEGER,
+    developer VARCHAR,
+    publisher VARCHAR,
+    screenshots TEXT,
+    play_time INTEGER,
+    completion_time INTEGER
+)
+
+-- subscription should be certain things only maybe? first off what is subscriptions for? the game or something else? what are the expected subcriptions to be ? are we checking if its true or 
