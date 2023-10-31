9e4d0442857443bb3dcab4f252ebf268c39ab40b
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air-8.lan>
2023-10-31 11:16:39 -0400

    created the basic structure of the game controller, made minor change in the schema as well

---
+++ controllers/gameController.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
--- db/schema.sql
+++ db/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE game (
     platform VARCHAR,
     boxart VARCHAR,
     ESRB VARCHAR,
-    subscription_service VARCHAR,
+    subscription VARCHAR,
     released_year INTEGER,
     developer VARCHAR,
     publisher VARCHAR,
@@ -23,5 +23,3 @@ CREATE TABLE game (
     play_time INTEGER,
     completion_time INTEGER
 )
-
--- subscription should be certain things only maybe? first off what is subscriptions for? the game or something else? what are the expected subcriptions to be ? are we checking if its true or 
