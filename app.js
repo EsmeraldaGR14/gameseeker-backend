3d31224e8cbe083b417a278033a1066e8619df8f
Irfan Hanjra <ihanjra@gmail.com>
2023-10-31 02:44:40 -0400

    Fixed typos in backend files

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to GameSeeker!");
+});
+
+app.get("*", (req, res) => {
+  res.send("Page not found");
 });
 
 module.exports = app;
