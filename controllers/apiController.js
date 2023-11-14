adf5423c314d9b2fab6a0ca68eea96f40eb46f0b
Irfan Hanjra <ihanjra@gmail.com>
2023-11-14 02:23:42 -0500

    Moved cors middleware

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,17 +3,6 @@ const router = express.Router();
 
 const { addGamesFromApisToDatabase } = require("../API/manipulateData");
 
-// router.get("/", async (req, res) => {
-//   try {
-//     const initialData = await addGamesFromApisToDatabase();
-
-//     res.json(initialData);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ status: 'error', message: 'Internal server error' });
-//   }
-// })
-
 router.post("/", async (req, res) => {
   try {
     
